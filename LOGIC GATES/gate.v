b952bbf6f95b12b5bb17ef31aac2b51fdafc6b9a
Lakshmivanapalli022 <vanapallilakshmi12@gmail.com>
2025-06-12 21:46:09 +0530

    Update logic_gates.v

--- LOGIC GATES/gate.v
+++ LOGIC GATES/gate.v
@@ -1,1 +1,10 @@
-
+module gates(
+  input a,b,
+  output y_and,y_or,y_not_a,y_xor,y_xnor
+);
+  and(y_and,a,b);
+  or(y_or,a,b);
+  not(y_not_a,a);
+  xor(y_xor,a,b);
+  xnor(y_xnor,a,b);
+endmodule
