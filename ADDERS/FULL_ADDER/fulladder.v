d16904e3dcd0513b82131cd66f1744edacd0ad08
Lakshmivanapalli022 <vanapallilakshmi12@gmail.com>
2025-06-12 23:21:44 +0530

    Update fulladder.v

--- ADDERS/FULL_ADDER/fulladder.v
+++ ADDERS/FULL_ADDER/fulladder.v
@@ -1,1 +1,12 @@
+module full_adder(
+    input A,
+    input B,
+    input C_in,
+    output SUM,
+    output C_out
+);
 
+assign SUM = A ^ B ^ C_in;    
+  assign C_out = (A & B) | (B & C_in) | (A & C_in);
+
+endmodule
